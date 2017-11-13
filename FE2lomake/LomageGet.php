05d302406413ffd7e44ca7c5d6627c9633d788c1
Jorgosgithub <33019328+Jorgosgithub@users.noreply.github.com>
2017-11-13 11:55:44 +0200

    Add files via upload

---
+++ FE2lomake/LomageGet.php
@@ -0,0 +1,10 @@
+<p><?php
+
+    echo $_GET["firstname"];?></p>
+echo $_GET["lastname"];
+echo $_GET["email"];
+echo $_GET["number"];
+echo $_GET["address"];
+echo $_GET["zip"];
+echo $_GET["password"];
+?>
---
+++ FE2lomake/LomagePost.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo $_POST["firstname"];
+echo $_POST["lastname"];
+echo $_POST["email"];
+echo $_POST["number"];
+echo $_POST["address"];
+echo $_POST["zip"];
+echo $_POST["password"];
+?>
